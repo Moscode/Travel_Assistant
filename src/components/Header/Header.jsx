d17cb67e05b2b1ba1da9d925629488c1203ac6bf
Moscode <odunoshomoses@gmail.com>
2022-02-09 18:24:51 +0000

    Navigation bar of the app is built using material-ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Box from '@material-ui/core/Box'
+import SearchIcon from '@material-ui/icons/Search'
+
+import useStyles from './HeaderStyles';
 
 function Header() {
+    const classes = useStyles();
+
     return ( 
-        <h1>Header</h1>
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant='h5' noWrap className={classes.title}>
+            Travel Assistant
+          </Typography>
+          <Box display='flex'>
+              <Typography variant='h6' className={classes.title}>
+                Explore new locations
+              </Typography>
+              <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder='Search...' classes={{
+                  root:classes.inputRoot,
+                  input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search'}}
+              />
+          </div>
+          </Box>
+         
+        </Toolbar>
+      </AppBar>
+    </div>
      );
 }
 
